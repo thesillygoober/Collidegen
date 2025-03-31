dd69763b608ab89bb58434050a670f37327b3a36
thesillygoober <70186143+thesillygoober@users.noreply.github.com>
2025-03-31 11:09:20 -0400

    Small amount of work
    Small amount of work just added the global.gd look at gemini for plans on how to load resources from the .tscn

--- Scenes/load_area.gd
+++ Scenes/load_area.gd
@@ -9,3 +9,10 @@ func _ready() -> void:
 func _on_open_dialog_button_down() -> void:
 	
 	file_dialog.visible = true
+
+func _on_file_selected(path: String) -> void:
+
+	
+	Global.tscn_load = ResourceLoader.load(path)
+	var scene = Global.tscn_load.instantiate()
+	add_child(scene)
---
+++ global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var tscn_load: PackedScene
